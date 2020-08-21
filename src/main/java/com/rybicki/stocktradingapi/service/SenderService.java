513a1c7b166cd6a050d06d7208cf47fc1ba52eb5
RybickiMichal <rybickimichal2@gmail.com>
2020-08-21 09:10:41 +0200

    add ListenableFuture to SenderService

--- src/main/java/com/rybicki/stocktradingapi/service/SenderService.java
+++ src/main/java/com/rybicki/stocktradingapi/service/SenderService.java
@@ -1,12 +1,17 @@
 package com.rybicki.stocktradingapi.service;
 
 import com.rybicki.stocktradingapi.model.OrderDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Service
+@Slf4j
 public class SenderService {
 
     @Autowired
@@ -16,6 +21,21 @@ public class SenderService {
     private String kafkaTopic;
 
     public void send(OrderDTO orderDTO) {
-        kafkaTemplate.send(kafkaTopic, orderDTO);
+        ListenableFuture<SendResult<String, OrderDTO>> future
+                = kafkaTemplate.send(kafkaTopic, orderDTO);
+
+        future.addCallback(new ListenableFutureCallback<>() {
+            @Override
+            public void onSuccess(SendResult<String, OrderDTO> result) {
+                log.info("Sent message: " + orderDTO.toString() + " with offset: " + result.getRecordMetadata().offset());
+            }
+
+            @Override
+            public void onFailure(Throwable ex) {
+                log.error("Unable to send message : " + orderDTO.toString(), ex);
+            }
+        });
+
+
     }
 }
